e07ef1b1842ed4928de07647d1dae5001c73ed62
Mustafa Türkmen <mustafa.muhammed@dop.com.tr>
2022-01-19 19:41:59 +0300

    enum ImageSizeId { mobile_list, mobile_list_col, mobile_detail, mobile_daily_menu } yeni Id eklendi

--- lib/src/enum/id_enum.dart
+++ lib/src/enum/id_enum.dart
@@ -54,7 +54,7 @@ enum MarketPlaceBottomButtonId { Home, Search, Campaign, Basket }
 /// [mobile_list] bu resim türü [ListMode.Line] inde kulanılmakta
 /// [mobile_list_col] bu resim türü [ListMode.Grid] inde kulanılmakta
 /// [mobile_detail] Ürün Detay ekranında kullanılmakta
-enum ImageSizeId { mobile_list, mobile_list_col, mobile_detail }
+enum ImageSizeId { mobile_list, mobile_list_col, mobile_detail, mobile_daily_menu }
 
 /// GetX için yazıldı
 /// Her bit item Getx Id sidir
