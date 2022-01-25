8b2eab8f502ec9fdf0df9e01b9f92028e0a188a0
Mustafa Türkmen <mustafa.muhammed@dop.com.tr>
2022-01-25 16:50:53 +0300

    RatingAndComment modelinde düzenleme yapıldı

--- lib/src/model/request/post_rating_and_comment.dart
+++ lib/src/model/request/post_rating_and_comment.dart
@@ -1,35 +1,51 @@
-
-
-
-class PostRatingAndComment {
-  PostRatingAndComment({
-    required this.subject,
-    required this.comment,
-    required this.ratings,
+class RatingAndComment {
+  RatingAndComment({
+    this.subject,
+    this.comment,
+    this.ratings,
+    this.createDate,
   });
 
-  String subject;
-  String comment;
-  List<Rating> ratings;
+  String? subject;
+  String? comment;
+  DateTime? createDate;
+  List<Rating>? ratings;
 
   Map<String, dynamic> toJson() => {
-    "subject": subject,
-    "comment": comment,
-    "ratings": List<dynamic>.from(ratings.map((x) => x.toJson())),
-  };
+        "subject": subject,
+        "comment": comment,
+        "create_date":
+            createDate == null ? null : createDate!.toIso8601String(),
+        "ratings": List<dynamic>.from(ratings!.map((x) => x.toJson())),
+      };
+
+  factory RatingAndComment.fromJson(Map<dynamic, dynamic> json) =>
+      RatingAndComment(
+        ratings: json["ratings"] == null ? null: List<Rating>.from(json["ratings"].map((x) => Rating.fromJson(x))),
+        subject: json["subject"],
+        createDate: json["create_date"] == null
+            ? null
+            : DateTime.parse(json["create_date"]),
+        comment: json["comment"],
+      );
 }
 
 class Rating {
   Rating({
-    required this.ratingCategory,
-    required this.ratingValue,
+    this.ratingCategory,
+    this.ratingValue,
   });
 
-  String ratingCategory;
-  int ratingValue;
+  String? ratingCategory;
+  int? ratingValue;
 
   Map<String, dynamic> toJson() => {
-    "rating_category": ratingCategory,
-    "rating_value": ratingValue,
-  };
+        "rating_category": ratingCategory,
+        "rating_value": ratingValue,
+      };
+
+  factory Rating.fromJson(Map<dynamic, dynamic> json) => Rating(
+        ratingCategory: json["rating_category"],
+        ratingValue: json["rating_value"],
+      );
 }
