17c687b32b06468aa01c728550c6bed70225303a
alper-mf <alper-mf@hotmail.com>
2022-02-12 00:25:51 +0300

    Update parameters_Model.dart

--- lib/src/model/response/parameters_Model.dart
+++ lib/src/model/response/parameters_Model.dart
@@ -4,11 +4,11 @@
 
 import 'dart:convert';
 
-import 'past_order_details_model.dart';
-import 'payment/payment_types_model.dart';
-
 ParametersModel parametersModelFromJson(String str) =>
     ParametersModel.fromJson(json.decode(str));
+
+String parametersModelToJson(ParametersModel data) =>
+    json.encode(data.toJson());
 
 class ParametersModel {
   ParametersModel({
@@ -20,28 +20,29 @@ class ParametersModel {
 
   List<AddressType>? addressType;
   List<FoodCategory>? foodCategories;
-  List<PaymentTypesModel>? paymentTypes;
+  List<PaymentType>? paymentTypes;
   List<OrderBy>? orderBy;
 
   factory ParametersModel.fromJson(Map<String, dynamic> json) =>
       ParametersModel(
-        addressType: json["address_type"] == null
-            ? []
-            : List<AddressType>.from(
-                json["address_type"].map((x) => AddressType.fromJson(x))),
-        foodCategories: json["food_categories"] == null
-            ? []
-            : List<FoodCategory>.from(
-                json["food_categories"].map((x) => FoodCategory.fromJson(x))),
-        paymentTypes: json["payment_types"] == null
-            ? []
-            : List<PaymentTypesModel>.from(json["payment_types"]
-                .map((x) => PaymentTypesModel.fromJson(x))),
-        orderBy: json["order_by"] == null
-            ? []
-            : List<OrderBy>.from(
-                json["order_by"].map((x) => OrderBy.fromJson(x))),
+        addressType: List<AddressType>.from(
+            json["address_type"].map((x) => AddressType.fromJson(x))),
+        foodCategories: List<FoodCategory>.from(
+            json["food_categories"].map((x) => FoodCategory.fromJson(x))),
+        paymentTypes: List<PaymentType>.from(
+            json["payment_types"].map((x) => PaymentType.fromJson(x))),
+        orderBy: List<OrderBy>.from(
+            json["order_by"].map((x) => OrderBy.fromJson(x))),
       );
+
+  Map<String, dynamic> toJson() => {
+        "address_type": List<dynamic>.from(addressType!.map((x) => x.toJson())),
+        "food_categories":
+            List<dynamic>.from(foodCategories!.map((x) => x.toJson())),
+        "payment_types":
+            List<dynamic>.from(paymentTypes!.map((x) => x.toJson())),
+        "order_by": List<dynamic>.from(orderBy!.map((x) => x.toJson())),
+      };
 }
 
 class AddressType {
@@ -67,6 +68,61 @@ class AddressType {
         "icon": icon,
       };
 }
+
+class FoodCategory {
+  FoodCategory(
+      {required this.categoryCode,
+      required this.categoryName,
+      required this.images,
+      this.isSelected});
+
+  String categoryCode;
+  String categoryName;
+  bool? isSelected;
+  Images images;
+
+  factory FoodCategory.fromJson(Map<String, dynamic> json) => FoodCategory(
+        categoryCode: json["category_code"],
+        categoryName: json["category_name"],
+        images: Images.fromJson(json["images"]),
+        isSelected: false,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "category_code": categoryCode,
+        "category_name": categoryName,
+        "images": images.toJson(),
+        'is_selected': isSelected,
+      };
+}
+
+class Images {
+  Images({
+    required this.imageUrl,
+    required this.imageSizeId,
+  });
+
+  String imageUrl;
+  ImageSizeId? imageSizeId;
+
+  factory Images.fromJson(Map<String, dynamic> json) => Images(
+        imageUrl: json["image_url"] == null ? null : json["image_url"],
+        imageSizeId: json["image_size_id"] == null
+            ? null
+            : imageSizeIdValues.map[json["image_size_id"]],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "image_url": imageUrl == null ? null : imageUrl,
+        "image_size_id":
+            imageSizeId == null ? null : imageSizeIdValues.reverse[imageSizeId],
+      };
+}
+
+enum ImageSizeId { MOBILE_KITCHEN }
+
+final imageSizeIdValues =
+    EnumValues({"mobile_kitchen": ImageSizeId.MOBILE_KITCHEN});
 
 class OrderBy {
   OrderBy({
