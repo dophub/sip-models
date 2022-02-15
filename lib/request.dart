70306e1d74345816f9cdca3ee084cfdc9dd813a1
Mustafa Türkmen <mustafa.muhammed@dop.com.tr>
2022-02-15 14:27:58 +0300

    create_reservation_model

--- lib/request.dart
+++ lib/request.dart
@@ -8,3 +8,4 @@ export 'src/model/request/payment/post_payment_model_by_saved_card.dart';
 export 'src/model/request/post_rating_and_comment.dart';
 export 'src/model/request/post_recommendations_model.dart';
 export 'src/model/request/update_user_model.dart';
+export 'src/model/request/create_reservation_model.dart';
---
+++ lib/src/model/request/create_reservation_model.dart
@@ -0,0 +1,46 @@
+class CrateReservationModel {
+  CrateReservationModel({
+    this.date,
+    this.personCount,
+    this.customerFullName,
+    this.customerPhone,
+    this.customerEmail,
+    this.customerNote,
+    this.fixMenu,
+  });
+
+  DateTime? date;
+  int? personCount;
+  String? customerFullName;
+  String? customerPhone;
+  String? customerEmail;
+  String? customerNote;
+  List<FixMenu>? fixMenu;
+
+  Map<String, dynamic> toJson() => {
+        "date": date?.toIso8601String(),
+        "person_count": personCount,
+        "customer_full_name": customerFullName,
+        "customer_phone": customerPhone,
+        "customer_email": customerEmail,
+        "customer_note": customerNote,
+        "fix_menu": fixMenu == null
+            ? null
+            : List<dynamic>.from(fixMenu!.map((x) => x.toJson())),
+      };
+}
+
+class FixMenu {
+  FixMenu({
+    this.id,
+    this.count,
+  });
+
+  int? id;
+  int? count;
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "count": count,
+      };
+}
