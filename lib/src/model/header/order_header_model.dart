f9b5272cac27255603ffb472b93a2f2618707dac
Mustafa Türkmen <mustafa.muhammed@dop.com.tr>
2022-02-25 10:02:21 +0300

    header modelde düzenleme

--- lib/src/model/header/order_header_model.dart
+++ lib/src/model/header/order_header_model.dart
@@ -3,27 +3,24 @@ import 'package:sip_models/enum.dart';
 import 'session_header_model.dart';
 
 class OrderHeaderModel extends SessionHeaderModel {
-  OrderHeaderModel(String token, this.sessionId, this.dealerId, {this.tableId})
-      : super(token);
-
-  /*{
-    //final sessionService = Get.find<SessionService>();
-    //sessionId = sessionService.getSessionId()!;
-  }*/
+  OrderHeaderModel(
+      String token, this.sessionId, this.dealerId, this._orderPoint,
+      {this.tableId})
+      : super(token: token, orderPoint: _orderPoint);
 
   String? tableId;
   int dealerId;
   late String sessionId;
-  late DeliveryType? deliveryType;
-  late OrderPoint? orderPoint;
+  late DeliveryType deliveryType;
+  late OrderPoint _orderPoint;
   late SessionPoint sessionPoint;
 
   Map<String, String> _toJson() => {
         "sessionid": sessionId,
         "dealerid": dealerId.toString(),
         "tableid": tableId ?? '',
-        "orderpoint": orderPoint == null ? '' : describeEnum(orderPoint!),
-        "deliverytype": deliveryType == null ? '' : describeEnum(deliveryType!),
+        "orderpoint": _orderPoint.name,
+        "deliverytype": deliveryType.name,
         "sessionpoint": describeEnum(sessionPoint),
       };
 
@@ -34,26 +31,22 @@ class OrderHeaderModel extends SessionHeaderModel {
 
   OrderHeaderModel _setDealer() {
     sessionPoint = SessionPoint.RESTIN;
-    orderPoint = OrderPoint.TABLE;
+    //_orderPoint = OrderPoint.TABLE;
     deliveryType = DeliveryType.TABLE;
     return this;
   }
 
-  OrderHeaderModel _setMarketPlace(
-      OrderPoint? _orderPoint, DeliveryType? _deliveryType) {
+  OrderHeaderModel _setMarketPlace(DeliveryType _deliveryType) {
     sessionPoint = SessionPoint.MARKETPLACE;
-    orderPoint = _orderPoint;
+    //_orderPoint = _orderPointt;
     deliveryType = _deliveryType;
     return this;
   }
 
   factory OrderHeaderModel.toDealer(
           String token, String sessionId, int dealerId, String tableId) =>
-      OrderHeaderModel(token, sessionId, dealerId, tableId: tableId)
-          ._setDealer();
+      OrderHeaderModel(token, sessionId, dealerId, OrderPoint.TABLE, tableId: tableId)._setDealer();
 
-  factory OrderHeaderModel.toMarketPlace(String token, String sessionId,
-          int dealerId, OrderPoint? _orderPoint, DeliveryType? _deliveryType) =>
-      OrderHeaderModel(token, sessionId, dealerId)
-          ._setMarketPlace(_orderPoint, _deliveryType);
+  factory OrderHeaderModel.toMarketPlace(String token, String sessionId, int dealerId, OrderPoint _orderPoint, DeliveryType _deliveryType) =>
+      OrderHeaderModel(token, sessionId, dealerId, _orderPoint)._setMarketPlace(_deliveryType);
 }
--- lib/src/model/header/session_header_model.dart
+++ lib/src/model/header/session_header_model.dart
@@ -1,24 +1,36 @@
+import 'package:sip_models/src/enum/id_enum.dart';
+
 ///  Business Logic'teki client class ları bu modelden extends edilecek.
 ///  Business Logic'teki Class türetildiği zaman bu modelin Constructor headerin gerekli parametrelerini dolduracak
 ///  daha sonra headere eklemek istediğimiz default parametreler hariç başka parametre eklemek istiyorsak
 ///  map paramtersine ekleyip [createHeader] metodunu çağırıyoruz.
 class SessionHeaderModel {
-  //final sessionService = Get.find<SessionService>();
+  SessionHeaderModel({
+    required this.token,
+    required this.orderPoint,
+    this.neighborhoodId = 0,
+    this.addressId = 0,
+    this.latlng = '0.0',
+  });
 
-  SessionHeaderModel(String token) {
-    authorization = 'Bearer $token';
-    lang = 'tr';
-  }
 
-  final String? contentType = "application/json";
-  String? authorization;
-  String? lang;
+  final String token;
+  final String contentType = "application/json";
+  final String lang = 'tr';
+  final OrderPoint orderPoint;
+  final int neighborhoodId;
+  final int addressId;
+  final String latlng;
 
   Map<String, String> createHeader({Map<String, String> addMap = const {}}) {
     final Map<String, String> _map = {
-      "content-type": contentType!,
-      "authorization": authorization!,
-      "lang": lang!,
+      "content-type": contentType,
+      "authorization": 'Bearer $token',
+      "lang": lang,
+      "order-point": orderPoint.name,
+      "neighborhood-id": neighborhoodId.toString(),
+      "address-id": addressId.toString(),
+      "latlng": latlng,
     };
     _map.addAll(addMap);
     return _map;
