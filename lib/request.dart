fbc913164b04e157e492a913d5d1cbf6ec23d06a
Mustafa Türkmen <mustafa.muhammed@dop.com.tr>
2022-03-05 17:26:58 +0300

    UpdateUserModel silindi

--- lib/request.dart
+++ lib/request.dart
@@ -7,6 +7,5 @@ export 'src/model/request/payment/post_payment_model_by_new_card.dart';
 export 'src/model/request/payment/post_payment_model_by_saved_card.dart';
 export 'src/model/request/post_rating_and_comment.dart';
 export 'src/model/request/post_recommendations_model.dart';
-export 'src/model/request/update_user_model.dart';
 export 'src/model/request/create_reservation_model.dart';
 export 'src/model/request/auth/verification_model.dart';
