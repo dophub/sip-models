3b21ce33b436d09e1a83d2ca06a72e3c4ba6776a
Mustafa Turkmen <mustafa.muhammed@dop.com.tr>
2022-03-31 17:36:43 +0300

    WaiterRequestModel edit

--- lib/src/model/response/waiter_request_model.dart
+++ lib/src/model/response/waiter_request_model.dart
@@ -7,15 +7,20 @@ List<WaiterRequestModel> waiterRequestModelFromJson(String str) =>
 /// Garson istek listesi
 class WaiterRequestModel {
   WaiterRequestModel(
-      {this.waiterSubjectCode, this.waiterSubjectName, this.status = false});
+      {this.waiterSubjectCode,
+      this.waiterSubjectName,
+      this.status = false,
+      this.imageUrl});
 
   String? waiterSubjectCode;
   String? waiterSubjectName;
   bool status; // chBox status u için kullanılmakta
+  String? imageUrl;
 
   factory WaiterRequestModel.fromJson(Map<String, dynamic> json) =>
       WaiterRequestModel(
         waiterSubjectCode: json["waiter_subject_code"],
         waiterSubjectName: json["waiter_subject_name"],
+        imageUrl: json["image_url"],
       );
 }
