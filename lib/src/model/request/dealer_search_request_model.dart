0cb52e41388f12e5113006f788a6a45a8a5b3fb0
Mustafa Turkmen <mustafa.muhammed@dop.com.tr>
2022-04-20 12:31:50 +0300

    Create dealer_search_request_model.dart

---
+++ lib/src/model/request/dealer_search_request_model.dart
@@ -0,0 +1,51 @@
+
+class DealerSearchRequestModel {
+  DealerSearchRequestModel({
+    this.keyword,
+    this.filter,
+    this.districtId,
+    this.neighborhoodId,
+    this.addressId,
+    this.latlng,
+  });
+
+  String? keyword;
+  ProductSearchFilterModel? filter;
+  int? districtId;
+  int? neighborhoodId;
+  String? addressId;
+  String? latlng;
+
+  Map<String, dynamic> toJson() => {
+    "keyword": keyword,
+    "filter": filter?.toJson(),
+    "district_id": districtId,
+    "neighborhood_id": neighborhoodId,
+    "address_id": addressId,
+    "latlng": latlng,
+  };
+}
+
+class ProductSearchFilterModel {
+  ProductSearchFilterModel({
+    this.foodCategory,
+    this.paymentType,
+    this.deliveryTime,
+    this.maxPackageAmount,
+    this.orderBy,
+  });
+
+  List<String>? foodCategory;
+  List<String>? paymentType;
+  int? deliveryTime;
+  int? maxPackageAmount;
+  String? orderBy;
+
+  Map<String, dynamic> toJson() => {
+    "food_category": List<dynamic>.from(foodCategory!.map((x) => x)),
+    "payment_type": List<dynamic>.from(paymentType!.map((x) => x)),
+    "delivery_time": deliveryTime,
+    "max_package_amount": maxPackageAmount,
+    "orderBy": orderBy,
+  };
+}
