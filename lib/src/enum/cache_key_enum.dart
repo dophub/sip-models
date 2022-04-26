436204e5c913469a6b82949524aa09b4dda73bbd
Mustafa Turkmen <mustafa.muhammed@dop.com.tr>
2022-04-26 12:03:27 +0300

    Update cache_key_enum.dart

--- lib/src/enum/cache_key_enum.dart
+++ lib/src/enum/cache_key_enum.dart
@@ -3,4 +3,4 @@
 /// kayıt edilen bilgilerin keyleri
 /// [Token] Kullanıcı token bilgileri.
 /// [ListMode] Liste modu, Kullanıcının son kullandığı liste modu kayıt edilmekte, Değeri [mode.ListMode] olması gerek.
-enum CacheKey { Token, ListMode, SessionId, Address, UserId, LoggedIn, Filter }
+enum CacheKey { Token, ListMode, SessionId, Address, LoggedIn, Filter }
