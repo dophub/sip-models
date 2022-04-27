2aecae9180d23da0d88b28472be0841a0d345d55
Alper Duzgun <alper.duzgun@dop.com.tr>
2022-04-27 14:52:04 +0300

    0427-CacheKey UserId Is Added

--- lib/src/enum/cache_key_enum.dart
+++ lib/src/enum/cache_key_enum.dart
@@ -1,6 +1,5 @@
-
 /// local storage kulanılmakta
 /// kayıt edilen bilgilerin keyleri
 /// [Token] Kullanıcı token bilgileri.
 /// [ListMode] Liste modu, Kullanıcının son kullandığı liste modu kayıt edilmekte, Değeri [mode.ListMode] olması gerek.
-enum CacheKey { Token, ListMode, SessionId, Address, LoggedIn, Filter }
+enum CacheKey { UserId, Token, ListMode, SessionId, Address, LoggedIn, Filter }
