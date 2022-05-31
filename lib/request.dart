60b1d6611b125558c91e8b9121f7296b256c8ca3
Mustafa Turkmen <mustafa.muhammed@dop.com.tr>
2022-05-31 17:07:32 +0300

    PostRecommendationsModel deleted

--- lib/request.dart
+++ lib/request.dart
@@ -6,7 +6,6 @@ export 'src/model/request/order_model.dart';
 export 'src/model/request/payment/post_payment_model_by_new_card.dart';
 export 'src/model/request/payment/post_payment_model_by_saved_card.dart';
 export 'src/model/request/post_rating_and_comment.dart';
-export 'src/model/request/post_recommendations_model.dart';
 export 'src/model/request/create_reservation_model.dart';
 export 'src/model/request/auth/verification_model.dart';
 export 'src/model/request/dealer_search_request_model.dart';
