5b9289f3e37985dae7ce6013ca05350f99f082a4
Mustafa Turkmen <49743631+Mto292@users.noreply.github.com>
2022-10-11 16:03:26 +0300

    payment_model_id added to dealerDetailModel

--- lib/src/model/response/dealer/dealer_detail_model.dart
+++ lib/src/model/response/dealer/dealer_detail_model.dart
@@ -1,7 +1,5 @@
-
 import '../other/food_category_model.dart';
 import '../other/images_model.dart';
-
 
 /// GR Kod okutulduğu zaman Api den dönen response
 class DealerDetailModel {
@@ -28,29 +26,21 @@ class DealerDetailModel {
   ImagesModel? listImage;
   ImagesModel? mobileCoverImage;
 
-  factory DealerDetailModel.fromJson(Map<String, dynamic> json) =>
-      DealerDetailModel(
+  factory DealerDetailModel.fromJson(Map<String, dynamic> json) => DealerDetailModel(
         dealerId: json["dealer_id"],
         mainBrandId: json["main_brand_id"],
         dealerName: json["dealer_name"],
         table: TableModel.fromJson(json["tables"]),
-        workingHours:  WorkingHourModel.fromJson(json["working_hours"]),
-        logoImage: json["logo_image"] == null
-            ? ImagesModel()
-            : ImagesModel.fromJson(json["logo_image"]),
-        listImage: json["list_image"] == null
-            ? ImagesModel()
-            : ImagesModel.fromJson(json["list_image"]),
-        mobileCoverImage: json["mobile_cover_image"] == null
-            ? ImagesModel()
-            : ImagesModel.fromJson(json["mobile_cover_image"]),
-        menus: json["menus"] == null
+        workingHours: WorkingHourModel.fromJson(json["working_hours"]),
+        logoImage: json["logo_image"] == null ? ImagesModel() : ImagesModel.fromJson(json["logo_image"]),
+        listImage: json["list_image"] == null ? ImagesModel() : ImagesModel.fromJson(json["list_image"]),
+        mobileCoverImage:
+            json["mobile_cover_image"] == null ? ImagesModel() : ImagesModel.fromJson(json["mobile_cover_image"]),
+        menus: json["menus"] == null ? [] : List<MenuModel>.from(json["menus"].map((x) => MenuModel.fromJson(x))),
+        categories: json["categories"] == null
             ? []
-            : List<MenuModel>.from(
-                json["menus"].map((x) => MenuModel.fromJson(x))),
-        categories: json["categories"] == null ? [] : List<FoodCategoryModel>.from(json["categories"].map((x) => FoodCategoryModel().fromJson(x))),
+            : List<FoodCategoryModel>.from(json["categories"].map((x) => FoodCategoryModel().fromJson(x))),
       );
-
 }
 
 /// Okutulan masa bilgileri
@@ -59,18 +49,23 @@ class TableModel {
     this.tableCode,
     this.tableName,
     this.stationCode,
+    this.stationName,
+    this.paymentModelId,
   });
 
   String? tableCode;
   String? tableName;
   String? stationCode;
+  String? stationName;
+  String? paymentModelId;
 
   factory TableModel.fromJson(Map<String, dynamic> json) => TableModel(
         tableCode: json["table_code"],
         tableName: json["table_name"],
         stationCode: json["station_code"],
+        stationName: json["station_name"],
+        paymentModelId: json["payment_model_id"],
       );
-
 }
 
 class MenuModel {
@@ -92,7 +87,6 @@ class MenuModel {
         menuImage: json["menu_image"],
         menuOrder: json["menu_order"],
       );
-
 }
 
 /// İşleteme çalışma saatleri
@@ -117,8 +111,7 @@ class WorkingHourModel {
   String? courierEnd;
   String? courierStart;
 
-  factory WorkingHourModel.fromJson(Map<String, dynamic> json) =>
-      WorkingHourModel(
+  factory WorkingHourModel.fromJson(Map<String, dynamic> json) => WorkingHourModel(
         end: json["end"],
         start: json["start"],
         title: json["title"],
@@ -128,6 +121,4 @@ class WorkingHourModel {
         courierEnd: json["courier_end"],
         courierStart: json["courier_start"],
       );
-
 }
-
