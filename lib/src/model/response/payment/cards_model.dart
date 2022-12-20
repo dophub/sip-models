3b89a3d45c4c700f6dc6bae3b127a6a1d717e764
Mustafa Turkmen <49743631+Mto292@users.noreply.github.com>
2022-12-20 16:27:47 +0300

    payeCard and mobile payment added

--- lib/src/model/response/payment/cards_model.dart
+++ lib/src/model/response/payment/cards_model.dart
@@ -33,19 +33,26 @@ class CardsIyzicoModel extends IBaseModel<CardsIyzicoModel> {
 }
 
 /// Paycell kullanıcı kayıtlı kartları çekmek için kullanılmakta
-
 class CardsModel {
   CardsModel({
     this.eulaId,
     this.cardList,
+    this.turkcellMobilePayment,
+    this.payeCard,
   });
 
   String? eulaId;
   List<CardModel>? cardList;
+  CardModel? payeCard;
+  TurkcellMobilePayment? turkcellMobilePayment;
 
   factory CardsModel.fromJson(Map<String, dynamic> json) => CardsModel(
         eulaId: json["eulaId"],
         cardList: List<CardModel>.from(json["cardList"].map((x) => CardModel.fromJson(x))),
+        turkcellMobilePayment:
+            json['mobilePayment'] == null ? null : TurkcellMobilePayment.fromJson(json['mobilePayment']),
+        payeCard:
+            json['payeCard'] == null ? null : CardModel.fromJson(json['payeCard']),
       );
 }
 
@@ -109,4 +116,40 @@ class CardModel extends ISectionsWidgetModel {
   set setStatus(bool value) {
     throw UnimplementedError();
   }
+}
+
+class TurkcellMobilePayment {
+  TurkcellMobilePayment({
+    this.cardLogo,
+    this.cardLimit,
+    this.isAvailable,
+    this.eulaId,
+    this.contractShow,
+    this.contractUrl,
+  });
+
+  String? cardLogo;
+  double? cardLimit;
+  bool? isAvailable;
+  String? eulaId;
+  bool? contractShow;
+  String? contractUrl;
+
+  factory TurkcellMobilePayment.fromJson(Map<String, dynamic> json) => TurkcellMobilePayment(
+        cardLogo: json["card_logo"],
+        cardLimit: json["card_limit"].toDouble(),
+        isAvailable: json["is_available"],
+        eulaId: json["eulaId"],
+        contractShow: json["contract_show"],
+        contractUrl: json["contract_url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "card_logo": cardLogo,
+        "card_limit": cardLimit,
+        "is_available": isAvailable,
+        "eulaId": eulaId,
+        "contract_show": contractShow,
+        "contract_url": contractUrl,
+      };
 }
