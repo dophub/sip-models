fdd5ce690a6553b35cf67184a7153b90dff2d19d
Mustafa Turkmen <49743631+Mto292@users.noreply.github.com>
2023-04-05 11:36:08 +0300

    feat:redirect model added

---
+++ lib/src/model/firebase/redirect_model.dart
@@ -0,0 +1,51 @@
+import 'package:background_json_parser/background_json_parser.dart';
+
+class RedirectModel extends IBaseModel<RedirectModel> {
+  RedirectModel({
+    required this.typeId,
+    required this.jsonData,
+  });
+
+  String typeId;
+  RedirectDataModel jsonData;
+
+  @override
+  RedirectModel fromJson(json) {
+    typeId = json["typeid"];
+    jsonData = RedirectDataModel().fromJson(json["json_data"]);
+    return this;
+  }
+
+  @override
+  Map<String, dynamic> toJson() => {
+        "typeid": typeId,
+        "json_data": jsonData.toJson(),
+      };
+}
+
+class RedirectDataModel extends IBaseModel<RedirectDataModel> {
+  RedirectDataModel({
+    this.module,
+    this.page,
+    this.qr,
+  });
+
+  String? module;
+  String? page;
+  String? qr;
+
+  @override
+  RedirectDataModel fromJson(Map<String, dynamic> json) {
+    module = json["module"];
+    page = json["page"];
+    qr = json["qr"];
+    return this;
+  }
+
+  @override
+  Map<String, dynamic> toJson() => {
+        "module": module,
+        "page": page,
+        "qr": qr,
+      };
+}
