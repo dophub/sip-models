e1355b3b58c7060fd56bd76a15b146e00c7490b3
Mustafa Turkmen <49743631+Mto292@users.noreply.github.com>
2023-04-05 11:36:47 +0300

    feat:redirect model added

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,3 +1,5 @@
 library firebase;
+
 export 'src/model/firebase/auth_session_model.dart';
 export 'src/model/firebase/payment_firebase_call_back_model.dart';
+export 'src/model/firebase/redirect_model.dart';
