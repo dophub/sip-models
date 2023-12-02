9eab85f762d8b65e3ba11a7eb6de7e463186708e
Mustafa Türkmen <mustafahussein292@yahoo.com>
2023-12-02 14:19:32 +0300

    feat

--- lib/src/enum/general_enum.dart
+++ lib/src/enum/general_enum.dart
@@ -9,21 +9,22 @@ enum AppEnvironment { Development, Staging, Production }
 /// [OUT_COMPLETE] Tamamlandı
 /// [OUT_CANCEL] İptal
 enum TakeOutOrderStatus {
-  OUT_TEMP('Cihazda', 0, 'Cihazda'),
-  OUT_WAIT('Onay Bekliyor', 1, 'Onay Bekliyor'),
-  OUT_ACCEPT('Onaylandı', 2, 'Onaylandı'),
-  OUT_KITCHEN('Hazırlanıyor', 3, 'Hazırlanıyor'),
-  OUT_READY('Hazırlandı', 4, 'Hazırlandı'),
-  OUT_ON_WAY('Yolda', 5, 'Yolda'),
-  OUT_DELIVERYOK('Teslim Edildi', 6, 'Teslim Edildi'),
-  OUT_COMPLETE('Tamamlandı', 7, 'Tamamlandı'),
-  OUT_CANCEL('İptal', 8, 'İptal');
+  OUT_TEMP('Cihazda', 0, 'Cihazda', false),
+  OUT_WAIT('Onay Bekliyor', 1, 'Onay Bekliyor', true),
+  OUT_ACCEPT('Onaylandı', 2, 'Onaylandı', true),
+  OUT_KITCHEN('Hazırlanıyor', 3, 'Hazırlanıyor', true),
+  OUT_READY('Hazırlandı', 4, 'Hazırlandı', true),
+  OUT_ON_WAY('Yolda', 5, 'Yolda', true),
+  OUT_DELIVERYOK('Teslim Edildi', 6, 'Teslim Edildi', true),
+  OUT_COMPLETE('Tamamlandı', 7, 'Tamamlandı', true),
+  OUT_CANCEL('İptal', 8, 'İptal', false);
 
   final int level;
   final String event;
   final String title;
+  final bool visibleForUser;
 
-  const TakeOutOrderStatus(this.title, this.level, this.event);
+  const TakeOutOrderStatus(this.title, this.level, this.event, this.visibleForUser);
 }
 
 /// [GET_TEMP] Cihazda
@@ -34,19 +35,20 @@ enum TakeOutOrderStatus {
 /// [GET_COMPLETE] Tamamlandı
 /// [GET_CANCEL] İptal
 enum GetInOrderStatus {
-  GET_TEMP('Cihazda', 0, 'Cihazda'),
-  GET_WAIT('Bekliyor', 1, 'Beklemede'),
-  GET_ACCEPT('Onaylandı', 2, 'Onaylandı'),
-  GET_KITCHEN('Hazırlanıyor', 3, 'Hazırlanıyor'),
-  GET_READY('Kasada Hazır', 4, 'Kasada Hazır'),
-  GET_COMPLETE('Tamamlandı', 5, 'Tamamlandı'),
-  GET_CANCEL('İptal', 6, 'İptal');
+  GET_TEMP('Cihazda', 0, 'Cihazda', false),
+  GET_WAIT('Bekliyor', 1, 'Beklemede', true),
+  GET_ACCEPT('Onaylandı', 2, 'Onaylandı', true),
+  GET_KITCHEN('Hazırlanıyor', 3, 'Hazırlanıyor', true),
+  GET_READY('Kasada Hazır', 4, 'Kasada Hazır', true),
+  GET_COMPLETE('Tamamlandı', 5, 'Tamamlandı', true),
+  GET_CANCEL('İptal', 6, 'İptal', false);
 
   final int level;
   final String event;
   final String title;
+  final bool visibleForUser;
 
-  const GetInOrderStatus(this.title, this.level, this.event);
+  const GetInOrderStatus(this.title, this.level, this.event, this.visibleForUser);
 }
 
 /// [IN_TEMP] Cihazda
@@ -57,38 +59,40 @@ enum GetInOrderStatus {
 /// [IN_COMPLETE] Tamamlandı
 /// [IN_CANCEL] İptal
 enum TableOrderStatus {
-  IN_TEMP('Cihazda', 1, 'Cihazda'),
-  IN_WAIT('Bekliyor', 2, 'Bekliyor'),
-  IN_ACCEPT('Onaylandı', 3, 'Onaylandı'),
-  IN_KITCHEN('Hazırlanıyor', 4, 'Hazırlanıyor'),
-  IN_TABLE('Masada', 5, 'Masada'),
-  IN_COMPLETE('Tamamlandı', 6, 'Tamamlandı'),
-  IN_CANCEL('İptal', 7, 'İptal');
+  IN_TEMP('Cihazda', 1, 'Cihazda', false),
+  IN_WAIT('Bekliyor', 2, 'Bekliyor', true),
+  IN_ACCEPT('Onaylandı', 3, 'Onaylandı', true),
+  IN_KITCHEN('Hazırlanıyor', 4, 'Hazırlanıyor', true),
+  IN_TABLE('Masada', 5, 'Masada', true),
+  IN_COMPLETE('Tamamlandı', 6, 'Tamamlandı', true),
+  IN_CANCEL('İptal', 7, 'İptal', false);
 
   final int level;
   final String event;
   final String title;
+  final bool visibleForUser;
 
-  const TableOrderStatus(this.title, this.level, this.event);
+  const TableOrderStatus(this.title, this.level, this.event, this.visibleForUser);
 }
 
 /// Service status id
 enum ServiceStatusEnum {
-  IN_TEMP('Cihazda', 1, 'Cihazda'),
-  IN_WAIT('Onay Bekliyor', 2, 'Onay Bekliyor'), //"Onay Bekliyor"
-  WAIT('Bekliyor', 3, 'Bekliyor'), //"Bekliyor"
-  IN_ACCEP('Onaylandı', 4, 'Onaylandı'), //"Onaylandı"
-  IN_KITCHEN('Hazırlanıyor', 5, 'Hazırlanıyor'), //"Hazırlanan Sipariş Var"
-  IN_TABLE('Masada', 6, 'Masada'), //"Siparişler Masada"
-  IN_COMPLETE('Tamamlandı', 7, 'Tamamlandı'), //Tüm Siparişler Ödendi"
-  IN_CANCEL('İptal Edildi', 8, 'İptal Edildi'), //"İptal Edildi"
-  CLOSE('Kapatıldı', 9, 'Kapatıldı'); //"Kapatıldı"
+  IN_TEMP('Cihazda', 1, 'Cihazda', false),
+  IN_WAIT('Onay Bekliyor', 2, 'Onay Bekliyor', true),
+  WAIT('Bekliyor', 3, 'Bekliyor', false),
+  IN_ACCEP('Onaylandı', 4, 'Onaylandı', true),
+  IN_KITCHEN('Hazırlanıyor', 5, 'Hazırlanıyor', true),
+  IN_TABLE('Masada', 6, 'Masada', true),
+  IN_COMPLETE('Tamamlandı', 7, 'Tamamlandı', true),
+  IN_CANCEL('İptal Edildi', 8, 'İptal Edildi', false),
+  CLOSE('Kapatıldı', 9, 'Kapatıldı', false);
 
   final int level;
   final String event;
   final String title;
+  final bool visibleForUser;
 
-  const ServiceStatusEnum(this.title, this.level, this.event);
+  const ServiceStatusEnum(this.title, this.level, this.event, this.visibleForUser);
 }
 
 /// [TimeoutAction] Dealer de Masa TimeOut durumu
