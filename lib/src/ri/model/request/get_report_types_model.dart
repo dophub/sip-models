6ac89840da300b4cc4dbf8e29f3a6e53c0fd9abe
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-08-28 09:36:38 +0300

    feat

--- lib/src/ri/model/request/get_report_types_model.dart
+++ lib/src/ri/model/request/get_report_types_model.dart
@@ -3,18 +3,18 @@ import 'package:background_json_parser/background_json_parser.dart';
 import '../../../../ri_models.dart';
 
 class GetReportTypesModel extends IBaseModel<GetReportTypesModel> implements IMultiItemPickerWidgetModel {
-  String reportTitle;
-  String reportId;
-  String endpointAddress;
-  String method;
-  GetReportTypesPayloadModel payload;
+  String? reportTitle;
+  String? reportId;
+  String? endpointAddress;
+  String? method;
+  GetReportTypesPayloadModel? payload;
 
   GetReportTypesModel({
-    required this.reportTitle,
-    required this.reportId,
-    required this.endpointAddress,
-    required this.method,
-    required this.payload,
+    this.reportTitle,
+    this.reportId,
+    this.endpointAddress,
+    this.method,
+    this.payload,
   });
 
   @override
@@ -23,7 +23,7 @@ class GetReportTypesModel extends IBaseModel<GetReportTypesModel> implements IMu
         reportId: json["report_id"],
         endpointAddress: json["endpoint_address"],
         method: json["method"],
-        payload: GetReportTypesPayloadModel.fromJson(json["payload"]),
+        payload: json["payload"] == null ? null : GetReportTypesPayloadModel.fromJson(json["payload"]),
       );
 
   @override
@@ -32,21 +32,21 @@ class GetReportTypesModel extends IBaseModel<GetReportTypesModel> implements IMu
         "report_id": reportId,
         "endpoint_address": endpointAddress,
         "method": method,
-        "payload": payload.toJson(),
+        "payload": payload?.toJson(),
       };
 
   @override
   bool selectedValue = false;
 
   @override
-  String get title => reportTitle;
+  String get title => reportTitle ?? '';
 }
 
 class GetReportTypesPayloadModel {
-  List<int> productIds;
-  List<String> status;
-  DateTime startDate;
-  DateTime endDate;
+  List<int>? productIds;
+  List<String>? status;
+  String? startDate;
+  String? endDate;
 
   GetReportTypesPayloadModel({
     required this.productIds,
@@ -56,16 +56,16 @@ class GetReportTypesPayloadModel {
   });
 
   factory GetReportTypesPayloadModel.fromJson(Map<String, dynamic> json) => GetReportTypesPayloadModel(
-        productIds: List<int>.from(json["product_ids"].map((x) => x)),
-        status: List<String>.from(json["status"].map((x) => x)),
-        startDate: DateTime.parse(json["start_date"]),
-        endDate: DateTime.parse(json["end_date"]),
+        productIds: json["product_ids"] == null ? [] : List<int>.from(json["product_ids"].map((x) => x)),
+        status: json["status"] == null ? [] : List<String>.from(json["status"].map((x) => x)),
+        startDate: json["start_date"],
+        endDate: json["end_date"],
       );
 
   Map<String, dynamic> toJson() => {
-        "product_ids": List<dynamic>.from(productIds.map((x) => x)),
-        "status": List<dynamic>.from(status.map((x) => x)),
-        "start_date": startDate.toIso8601String(),
-        "end_date": endDate.toIso8601String(),
+        "product_ids": List<dynamic>.from(productIds!.map((x) => x)),
+        "status": List<dynamic>.from(status!.map((x) => x)),
+        "start_date": startDate,
+        "end_date": endDate,
       };
 }
