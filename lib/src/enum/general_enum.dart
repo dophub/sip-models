c86c949abf3f068eb741a74f3dbcee516f908878
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-09-09 13:06:28 +0300

    feat

--- lib/src/enum/general_enum.dart
+++ lib/src/enum/general_enum.dart
@@ -15,10 +15,10 @@ enum TakeOutOrderStatus {
   OUT_KITCHEN('Hazırlanıyor', 3, 'Mutfağa Gönder', true, Duration(minutes: 0), 'Depoya Gönder'),
   OUT_READY('Hazırlandı', 4, 'Hazırlandı', true, Duration(minutes: 4), 'Hazırlandı'),
   OUT_ON_WAY('Yolda', 5, 'Kuryeye Verildi', true, Duration(minutes: 4), 'sevkiyata Verildi'),
-  OUT_DELIVERYOK('Teslim Edildi', 6, 'Teslim Edildi', true, Duration(minutes: 4), 'Teslim Edildi'),
-  OUT_COMPLETE('Tamamlandı', 7, 'Tamamlandı', true, Duration(minutes: 4), 'Tamamlandı'),
-  OUT_CANCEL('İptal', 8, 'İptal', false, Duration(minutes: 4), 'İptal'),
-  OUT_REJECT('Reddedildi', 9, 'Reddedildi', false, Duration(minutes: 4), 'Reddedildi');
+  OUT_DELIVERYOK('Teslim Edildi', 6, 'Teslim Edildi', true, Duration(minutes: 5), 'Teslim Edildi'),
+  OUT_COMPLETE('Tamamlandı', 7, 'Tamamlandı', true, Duration(minutes: 5), 'Tamamlandı'),
+  OUT_CANCEL('İptal', 8, 'İptal', false, Duration(minutes: 5), 'İptal'),
+  OUT_REJECT('Reddedildi', 9, 'Reddedildi', false, Duration(minutes: 5), 'Reddedildi');
 
   final int level;
   final String event;
