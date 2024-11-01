0b580df278a8c14cf158c4d5cde1c5901fec3308
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-11-01 17:12:57 +0300

    feat

--- lib/src/ri/model/response/other/my_device_status_model.dart
+++ lib/src/ri/model/response/other/my_device_status_model.dart
@@ -1,43 +1,119 @@
 import 'package:background_json_parser/background_json_parser.dart';
 
 class MyDeviceStatusModel extends IBaseModel<MyDeviceStatusModel> {
-  bool? status;
-  String? type;
-  String? id;
-  dynamic createdAt;
-  String? dealerName;
-  String? title;
-  MyDeviceStatusModel? options;
+  final int? brandId;
+  final String? brandName;
+  final List<MyDeviceStatusDataModel>? data;
 
   MyDeviceStatusModel({
-    this.status,
-    this.type,
-    this.id,
-    this.createdAt,
-    this.dealerName,
-    this.title,
-    this.options,
+    this.brandId,
+    this.brandName,
+    this.data,
   });
 
   @override
   fromJson(Map<String, dynamic> json) => MyDeviceStatusModel(
-        status: json["status"],
-        type: json["type"],
-        id: json["id"],
-        createdAt: json["created_at"],
-        dealerName: json["dealer_name"],
-        title: json["title"],
-        options: json["options"] == null ? null : MyDeviceStatusModel().fromJson(json["options"]),
+        brandId: json["brand_id"],
+        brandName: json["brand_name"],
+        data: json["data"] == null ? [] : List<MyDeviceStatusDataModel>.from(json["data"]!.map((x) => MyDeviceStatusDataModel.fromJson(x))),
       );
 
   @override
   Map<String, dynamic> toJson() => {
+        "brand_id": brandId,
+        "brand_name": brandName,
+        "data": data == null ? [] : List<dynamic>.from(data!.map((x) => x.toJson())),
+      };
+}
+
+class MyDeviceStatusDataModel {
+  final bool? status;
+  final Type? type;
+  final int? dealerId;
+  final String? id;
+  final String? createdAt;
+  final String? dealerName;
+  final int? brandId;
+  final String? brandName;
+  final String? title;
+  final MyDeviceStatusOptionsModel? options;
+
+  MyDeviceStatusDataModel({
+    this.status,
+    this.type,
+    this.dealerId,
+    this.id,
+    this.createdAt,
+    this.dealerName,
+    this.brandId,
+    this.brandName,
+    this.title,
+    this.options,
+  });
+
+  factory MyDeviceStatusDataModel.fromJson(Map<String, dynamic> json) => MyDeviceStatusDataModel(
+        status: json["status"],
+        type: json["type"],
+        dealerId: json["dealer_id"],
+        id: json["id"],
+        createdAt: json["created_at"],
+        dealerName: json["dealer_name"],
+        brandId: json["brand_id"],
+        brandName: json["brand_name"],
+        title: json["title"],
+        options: json["options"] == null ? null : MyDeviceStatusOptionsModel.fromJson(json["options"]),
+      );
+
+  Map<String, dynamic> toJson() => {
         "status": status,
         "type": type,
+        "dealer_id": dealerId,
+        "id": id,
+        "created_at": createdAt,
+        "dealer_name": dealerName,
+        "brand_id": brandId,
+        "brand_name": brandName,
+        "title": title,
+        "options": options?.toJson(),
+      };
+}
+
+class MyDeviceStatusOptionsModel {
+  final bool? status;
+  final String? type;
+  final int? dealerId;
+  final String? id;
+  final String? createdAt;
+  final String? dealerName;
+  final String? title;
+
+  MyDeviceStatusOptionsModel({
+    this.status,
+    this.type,
+    this.dealerId,
+    this.id,
+    this.createdAt,
+    this.dealerName,
+    this.title,
+  });
+
+  factory MyDeviceStatusOptionsModel.fromJson(Map<String, dynamic> json) => MyDeviceStatusOptionsModel(
+        status: json["status"],
+        type: json["type"],
+        dealerId: json["dealer_id"],
+        id: json["id"],
+        createdAt: json["created_at"],
+        dealerName: json["dealer_name"],
+        title: json["title"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+        "type": type,
+        "dealer_id": dealerId,
         "id": id,
         "created_at": createdAt,
         "dealer_name": dealerName,
         "title": title,
-        "options": options?.toJson(),
       };
 }
