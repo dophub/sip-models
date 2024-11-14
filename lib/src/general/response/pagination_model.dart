141b9a316065682d794425d1316b8f95a5cc3484
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-11-14 12:54:31 +0300

    feat

--- lib/src/general/response/pagination_model.dart
+++ lib/src/general/response/pagination_model.dart
@@ -1,41 +1,36 @@
 import 'package:background_json_parser/background_json_parser.dart';
 
 class PaginationModel<T extends IBaseModel> extends IBaseModel {
-  final T? data;
-  final int? page;
-  final int? limit;
-  final int? totalCount;
-  final int? totalPage;
-  final int? lastPage;
-  final int? nextPage;
-  final int? prevPage;
+  late final T _modelParser;
+  List<T>? data;
+  int? page;
+  int? limit;
+  int? totalCount;
+  int? totalPage;
+  int? lastPage;
+  int? nextPage;
+  int? prevPage;
 
-  PaginationModel({
-    required this.data,
-    this.page,
-    this.limit,
-    this.totalCount,
-    this.totalPage,
-    this.lastPage,
-    this.nextPage,
-    this.prevPage,
-  });
+  PaginationModel(T modelParser) {
+    _modelParser = modelParser;
+  }
 
   @override
-  fromJson(Map<String, dynamic> json) => PaginationModel<T>(
-        data: json["data"] == null ? null : data?.fromJson(json["data"]),
-        page: json["page"],
-        limit: json["limit"],
-        totalCount: json["total_count"],
-        totalPage: json["total_page"],
-        lastPage: json["last_page"],
-        nextPage: json["next_page"],
-        prevPage: json["prev_page"],
-      );
+  PaginationModel<T> fromJson(Map<String, dynamic> json) {
+    data = json["data"] == null ? null : _modelParser.jsonParser(json["data"]);
+    page = json["page"];
+    limit = json["limit"];
+    totalCount = json["total_count"];
+    totalPage = json["total_page"];
+    lastPage = json["last_page"];
+    nextPage = json["next_page"];
+    prevPage = json["prev_page"];
+    return this;
+  }
 
   @override
   Map<String, dynamic> toJson() => {
-        "data": data?.toJson(),
+        "data": _modelParser.convertToJson(data ?? []),
         "page": page,
         "limit": limit,
         "total_count": totalCount,
