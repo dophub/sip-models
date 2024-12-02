d3d88c0edff3d05185958416df90a3504646fc16
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-12-02 19:11:20 +0300

    feat

--- lib/ri_models.dart
+++ lib/ri_models.dart
@@ -28,6 +28,7 @@ export 'package:sip_models/src/ri/model/response/login/partnership_form_model.da
 export 'package:sip_models/src/ri/model/response/login/partnership_options_model.dart';
 export 'package:sip_models/src/ri/model/response/login/transaction_model.dart';
 export 'package:sip_models/src/ri/model/response/order/kitchen_order_model.dart';
+export 'package:sip_models/src/ri/model/response/order/order_status_history_model.dart';
 export 'package:sip_models/src/ri/model/response/order/marketplace_order_model.dart';
 export 'package:sip_models/src/ri/model/response/order/order_manager_model.dart';
 export 'package:sip_models/src/ri/model/response/order/past_order_for_invoice.dart';
---
+++ lib/src/ri/model/response/order/order_status_history_model.dart
@@ -0,0 +1,71 @@
+import 'package:background_json_parser/background_json_parser.dart';
+
+class OrderStatusHistoryModel extends IBaseModel<OrderStatusHistoryModel> {
+  final OrderStatusHistoryStatusModel prevOrderStatus;
+  final OrderStatusHistoryStatusModel currentOrderStatus;
+  final DateTime updateDate;
+  final OrderStatusHistoryUserModel orderHistoryCustomer;
+
+  OrderStatusHistoryModel({
+    required this.prevOrderStatus,
+    required this.currentOrderStatus,
+    required this.updateDate,
+    required this.orderHistoryCustomer,
+  });
+
+  @override
+  fromJson(Map<String, dynamic> json) => OrderStatusHistoryModel(
+        prevOrderStatus: OrderStatusHistoryStatusModel.fromJson(json["prev_order_status"]),
+        currentOrderStatus: OrderStatusHistoryStatusModel.fromJson(json["current_order_status"]),
+        updateDate: DateTime.parse(json["update_date"]),
+        orderHistoryCustomer: OrderStatusHistoryUserModel.fromJson(json["order_history_customer"]),
+      );
+
+  @override
+  Map<String, dynamic> toJson() => {
+        "prev_order_status": prevOrderStatus.toJson(),
+        "current_order_status": currentOrderStatus.toJson(),
+        "update_date": updateDate.toIso8601String(),
+        "order_history_customer": orderHistoryCustomer.toJson(),
+      };
+}
+
+class OrderStatusHistoryStatusModel {
+  final String code;
+  final String name;
+
+  OrderStatusHistoryStatusModel({
+    required this.code,
+    required this.name,
+  });
+
+  factory OrderStatusHistoryStatusModel.fromJson(Map<String, dynamic> json) => OrderStatusHistoryStatusModel(
+        code: json["code"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "code": code,
+        "name": name,
+      };
+}
+
+class OrderStatusHistoryUserModel {
+  final String firstname;
+  final String lastname;
+
+  OrderStatusHistoryUserModel({
+    required this.firstname,
+    required this.lastname,
+  });
+
+  factory OrderStatusHistoryUserModel.fromJson(Map<String, dynamic> json) => OrderStatusHistoryUserModel(
+        firstname: json["firstname"],
+        lastname: json["lastname"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "firstname": firstname,
+        "lastname": lastname,
+      };
+}
