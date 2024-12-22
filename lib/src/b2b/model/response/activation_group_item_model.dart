b6bb4b1adfa9998a6ba01127e0580793c6611cd4
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-12-22 17:33:44 +0300

    feat

--- lib/src/b2b/model/response/activation_group_item_model.dart
+++ lib/src/b2b/model/response/activation_group_item_model.dart
@@ -3,6 +3,7 @@ import 'package:background_json_parser/background_json_parser.dart';
 class ActivationGroupItemModel extends IBaseModel<ActivationGroupItemModel> {
   int? id;
   String? itemTypeId;
+  String? name;
   int? productId;
   int? promotionMenuId;
   int? activationGroupId;
@@ -11,6 +12,7 @@ class ActivationGroupItemModel extends IBaseModel<ActivationGroupItemModel> {
   ActivationGroupItemModel({
     this.id,
     this.itemTypeId,
+    this.name,
     this.productId,
     this.promotionMenuId,
     this.activationGroupId,
@@ -21,6 +23,7 @@ class ActivationGroupItemModel extends IBaseModel<ActivationGroupItemModel> {
   fromJson(Map<String, dynamic> json) => ActivationGroupItemModel(
         id: json["id"],
         itemTypeId: json["item_type_id"],
+        name: json["name"],
         productId: json["product_id"],
         promotionMenuId: json["promotion_menu_id"],
         activationGroupId: json["activation_group_id"],
@@ -32,6 +35,7 @@ class ActivationGroupItemModel extends IBaseModel<ActivationGroupItemModel> {
         "id": id,
         "item_type_id": itemTypeId,
         "product_id": productId,
+        "name": name,
         "promotion_menu_id": promotionMenuId,
         "activation_group_id": activationGroupId,
         "item": item?.toJson(),
