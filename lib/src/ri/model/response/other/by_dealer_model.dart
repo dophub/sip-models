9eadd184316d08a5073c7ff8c53f99ad7fb264aa
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-12-31 12:05:46 +0300

    feat

--- lib/src/ri/model/response/other/by_dealer_model.dart
+++ lib/src/ri/model/response/other/by_dealer_model.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:background_json_parser/background_json_parser.dart';
 
 class ByDealerModel<T extends IBaseModel<T>> {
-  List<T>? dealerData;
+  ByDealerDataModel<T>? dealerData;
   List<T>? totalData;
 
   ByDealerModel({
@@ -14,8 +14,46 @@ class ByDealerModel<T extends IBaseModel<T>> {
   factory ByDealerModel.fromJson(T model, String json) {
     final map = jsonDecode(json);
     return ByDealerModel<T>(
-      dealerData: map["dealer_data"] == null ? [] : model.jsonParser(map["dealer_data"]),
+      dealerData: map["dealer_data"] == null ? null : ByDealerDataModel<T>.fromJson(model, map["dealer_data"]),
       totalData: map["total_data"] == null ? [] : model.jsonParser(map["dealer_data"]),
     );
   }
+}
+
+class ByDealerDataModel<T extends IBaseModel<T>> {
+  ByDealerDealerModel? dealer;
+  List<T>? dealerData;
+
+  ByDealerDataModel({
+    this.dealer,
+    this.dealerData,
+  });
+
+  factory ByDealerDataModel.fromJson(T model, String json) {
+    final map = jsonDecode(json);
+    return ByDealerDataModel(
+      dealer: map["dealer"] == null ? null : ByDealerDealerModel.fromJson(map["dealer"]),
+      dealerData: map["dealer_data"] == null ? null : model.jsonParser(map["dealer_data"]),
+    );
+  }
+}
+
+class ByDealerDealerModel {
+  int? id;
+  String? name;
+
+  ByDealerDealerModel({
+    this.id,
+    this.name,
+  });
+
+  factory ByDealerDealerModel.fromJson(Map<String, dynamic> json) => ByDealerDealerModel(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
 }
