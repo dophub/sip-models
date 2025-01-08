bcecc5a1725bcc2a1c662bb9979e8ac99f6b21d7
Mustafa Türkmen <mustafahussein292@yahoo.com>
2025-01-08 15:51:01 +0300

    feat

--- lib/src/ri/model/request/update_price_model_req.dart
+++ lib/src/ri/model/request/update_price_model_req.dart
@@ -1,17 +1,32 @@
 class UpdatePriceRequestModel {
   UpdatePriceRequestModel({
     required this.productId,
-    required this.price,
+    required this.prices,
     required this.itemTypeId,
   });
 
   int productId;
-  double price;
+  List<UpdatePriceListRequestModel> prices;
   String itemTypeId;
 
   Map<String, dynamic> toJson() => {
         "product_id": productId,
-        "price": price,
+        "prices": prices.map((e) => e.toJson()),
         "item_type_id": itemTypeId,
+      };
+}
+
+class UpdatePriceListRequestModel {
+  UpdatePriceListRequestModel({
+    required this.price,
+    required this.orderDeliveryTypeId,
+  });
+
+  double price;
+  String orderDeliveryTypeId;
+
+  Map<String, dynamic> toJson() => {
+        "price": price,
+        "order_delivery_type_id": orderDeliveryTypeId,
       };
 }
