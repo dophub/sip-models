e10f06e748db16ff5e875911c418fb61583d8605
Mustafa Türkmen <mustafahussein292@yahoo.com>
2025-06-19 11:56:30 +0300

    feat

--- lib/src/model/response/campaign/campaign_model.dart
+++ lib/src/model/response/campaign/campaign_model.dart
@@ -20,6 +20,7 @@ class CampaignModel extends IBaseModel<CampaignModel> {
     this.limitNumberOfOrderTypeId,
     this.dealers,
     this.orderPoints,
+    this.banner,
   });
 
   int? id;
@@ -38,6 +39,7 @@ class CampaignModel extends IBaseModel<CampaignModel> {
   String? limitNumberOfOrderTypeId;
   List<CampaignDealerModel>? dealers;
   List<String>? orderPoints;
+  String? banner;
 
   @override
   CampaignModel fromJson(Map<dynamic, dynamic> json) {
@@ -61,8 +63,11 @@ class CampaignModel extends IBaseModel<CampaignModel> {
         campaignDiscountPercent: json["campaign_discount_percent"].toDouble(),
         notUseBannerForTemplate: json["not_use_banner_for_template"],
         isOtpRequired: json["is_otp_required"],
-        dealers: json["dealers"] == null ? [] : List<CampaignDealerModel>.from(json["dealers"]!.map((x) => CampaignDealerModel.fromJson(x))),
+        dealers: json["dealers"] == null
+            ? []
+            : List<CampaignDealerModel>.from(json["dealers"]!.map((x) => CampaignDealerModel.fromJson(x))),
         orderPoints: json["order_points"] == null ? [] : List<String>.from(json["order_points"]!.map((x) => x)),
+        banner: json["banner"],
       );
 
   @override
@@ -82,6 +87,7 @@ class CampaignModel extends IBaseModel<CampaignModel> {
         "limit_number_of_order_type_id": limitNumberOfOrderTypeId,
         "dealers": dealers == null ? [] : List<dynamic>.from(dealers!.map((x) => x.toJson())),
         "order_points": orderPoints == null ? [] : List<dynamic>.from(orderPoints!.map((x) => x)),
+        "banner": banner,
       };
 }
 
