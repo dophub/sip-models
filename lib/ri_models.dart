45f1a2654c3f8b7b4883afd76402392d0c047671
Mustafa Türkmen <mustafahussein292@yahoo.com>
2025-08-14 15:02:46 +0300

    feat

--- lib/ri_models.dart
+++ lib/ri_models.dart
@@ -15,6 +15,7 @@ export 'package:sip_models/src/ri/model/request/update_payment_type_request_mode
 export 'package:sip_models/src/ri/model/request/update_price_model_req.dart';
 export 'package:sip_models/src/ri/model/request/update_product_request_model.dart';
 export 'package:sip_models/src/ri/model/request/update_tip_system_request_model.dart';
+export 'package:sip_models/src/ri/model/request/start_payment_model.dart';
 export 'package:sip_models/src/ri/model/response/courier/courier_order_model.dart';
 export 'package:sip_models/src/ri/model/response/customer/add_local_customer_model.dart';
 export 'package:sip_models/src/ri/model/response/customer/customer_review_detail_model.dart';
@@ -56,6 +57,7 @@ export 'package:sip_models/src/ri/model/response/other/start_order_cancel_verify
 export 'package:sip_models/src/ri/model/response/other/tip_list_response_model.dart';
 export 'package:sip_models/src/ri/model/response/other/tip_system_detail_response_model.dart';
 export 'package:sip_models/src/ri/model/response/other/update_branch_product_activation_model.dart';
+export 'package:sip_models/src/ri/model/response/other/physical_pos_model.dart';
 export 'package:sip_models/src/ri/model/response/parameters/courier_model.dart';
 export 'package:sip_models/src/ri/model/response/parameters/order_cancel_model.dart';
 export 'package:sip_models/src/ri/model/response/parameters/order_status_model.dart';
