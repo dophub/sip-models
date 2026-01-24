cf8d5e7abddbe3391e37ff245f07440789fb56b0
Mustafa Türkmen <mustafahussein292@yahoo.com>
2026-01-24 05:07:34 +0300

    feat

--- lib/response.dart
+++ lib/response.dart
@@ -47,5 +47,6 @@ export 'src/model/response/order/past_service_detail_model.dart';
 export 'src/model/response/other/wallet_qr_detail_model.dart';
 export 'src/model/response/other/wallet_qr_payment_operation_model.dart';
 export 'src/model/response/other/fast_sale_close_table_model.dart';
+export 'src/model/response/other/profile_dashboard_model.dart';
 export 'src/model/response/address/lat_lng_model.dart';
 export 'package:sip_models/src/general/response/start_payment_response_model.dart';
