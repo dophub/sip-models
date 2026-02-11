aa0b12ad41af0fb0721a3b31814dbda0280b615e
Mustafa Türkmen <mustafahussein292@yahoo.com>
2026-02-11 19:22:52 +0300

    feat

--- lib/src/ri/model/response/station/service_model.dart
+++ lib/src/ri/model/response/station/service_model.dart
@@ -6,13 +6,15 @@ class StationServiceModel {
   num? total;
   String? createSideId;
   TableOrderStatusModel? status;
+  double? paymentAmount;
 
   StationServiceModel({
     this.id,
     this.person,
-    this.total,
+    this.total, // toplam masa tutarı
     this.createSideId,
     this.status,
+    this.paymentAmount, // Ödenen tutar
   });
 
   factory StationServiceModel.fromJson(Map<String, dynamic> json) => StationServiceModel(
@@ -21,6 +23,7 @@ class StationServiceModel {
         total: json['total'] as num?,
         createSideId: json['create_side_id'] as String?,
         status: json['status'] == null ? null : TableOrderStatusModel.fromJson(json['status'] as Map<String, dynamic>),
+        paymentAmount: json['payment_amount']?.toDouble(),
       );
 
   Map<String, dynamic> toJson() => {
@@ -29,5 +32,6 @@ class StationServiceModel {
         'total': total,
         'create_side_id': createSideId,
         'status': status?.toJson(),
+        'payment_amount': paymentAmount,
       };
 }
