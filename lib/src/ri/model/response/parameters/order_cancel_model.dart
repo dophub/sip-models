06ade46a53f088767e610329c43a6e3032427c3e
Mustafa Türkmen <mustafahussein292@yahoo.com>
2026-08-19 16:45:01 +0300

    feat: add cancel_reasons

--- lib/src/ri/model/response/parameters/order_cancel_model.dart
+++ lib/src/ri/model/response/parameters/order_cancel_model.dart
@@ -1,18 +1,25 @@
-
 class OrderCancelModel {
+  String? type;
   String? codeTitle;
   String? cancelCode;
   String? cancelOrReject;
 
-  OrderCancelModel({this.codeTitle, this.cancelCode, this.cancelOrReject});
+  OrderCancelModel({
+    this.type,
+    this.codeTitle,
+    this.cancelCode,
+    this.cancelOrReject,
+  });
 
   factory OrderCancelModel.fromJson(Map<String, dynamic> json) => OrderCancelModel(
+        type: json['type'],
         codeTitle: json['code_title'],
         cancelCode: json['cancel_code'],
         cancelOrReject: json['cancel_reject'], //'C':Cancel, 'R':Reject
       );
 
   Map<String, dynamic> toJson() => {
+        'type': type,
         'code_title': codeTitle,
         'cancel_code': cancelCode,
         'cancel_reject': cancelOrReject,
