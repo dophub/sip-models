31a4b23bc458de10061d1587f53ba56259e1d589
Mustafa Türkmen <mustafahussein292@yahoo.com>
2026-09-22 21:28:33 +0300

    feat: change IdNameModel to primary constructor

--- lib/src/ri/model/response/other/id_name_model.dart
+++ lib/src/ri/model/response/other/id_name_model.dart
@@ -1,23 +1,12 @@
 import 'package:background_json_parser/background_json_parser.dart';
 
-class IdNameModel extends IBaseModel<IdNameModel> {
+class IdNameModel({required int id, required int name}) extends IBaseModel<IdNameModel> {
   int? id;
   String? name;
 
-  IdNameModel({
-    this.id,
-    this.name,
-  });
+  @override
+  fromJson(Map<String, dynamic> json) => IdNameModel(id: json["id"], name: json["name"]);
 
   @override
-  fromJson(Map<String, dynamic> json) => IdNameModel(
-        id: json["id"],
-        name: json["name"],
-      );
-
-  @override
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-      };
+  Map<String, dynamic> toJson() => {"id": id, "name": name};
 }
